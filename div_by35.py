feae399f503761151c18af0efce71ca6543bba97
SwarnenduR <42539048+SwarnenduR@users.noreply.github.com>
2025-03-12 14:39:09 +0530

    stock market analysis files

---
+++ div_by35.py
@@ -0,0 +1,8 @@
+
+num = 50
+if num // 3 == 0:
+    print ("number divisible by 3")
+if num // 5 == 0:
+    print ("number divisible by 5")
+if (num // 3 == 0 and num // 5 == 0):
+    print ("number divisible by 3 and 5")
